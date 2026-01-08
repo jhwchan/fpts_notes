8f8895a789fb93f064cc3afc0ba90ba828833198
Jason Chan <jchan@lunarenergy.com>
2026-01-08 14:43:54 +0000

    a

--- io-ts/codecs.ts
+++ io-ts/codecs.ts
@@ -74,7 +74,6 @@ const StringFromNumber = new t.Type<number, string, any>(
  *   - converts a variable of type { age: number, username: Option<Branded Username> } into { age: number, username: Branded Username }
  */
 
-//rand
 const complexCodec2 = tt.fromNullable(
     t.strict({
         age: tt.fromNullable(StringFromNumber, 0),
